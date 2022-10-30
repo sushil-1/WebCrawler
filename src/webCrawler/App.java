b2425e04dfb93faad563e561579471b4af19cd12
unknown <BizAct-181@BizAct181.bizacuity.local>
2022-10-30 10:29:13 +0530

    added comment

--- src/webCrawler/App.java
+++ src/webCrawler/App.java
@@ -3,6 +3,7 @@ package webCrawler;
 public class App {
 
     public static void main(String[] args) {
+    	// new web crawler
         WebCrawler crawler = new WebCrawler();
         String rootURL = "https://www.facebook.com/login";
         crawler.crawl(rootURL, 100);
